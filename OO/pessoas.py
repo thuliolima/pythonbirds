6ebbaf55193e32cd5be018f2cec204132ec9cb65
Thulio Vinicius <thuliov@gmail.com>
2020-11-20 18:49:32 -0300

    Criando atributos de idade e nome.

--- OO/pessoas.py
+++ OO/pessoas.py
@@ -1,2 +1,17 @@
 class Pessoa:
-    pass
+    def __init__(self, name=None, idade=29):
+        self.name = name
+        self.idade = idade
+
+
+    def cumprimentar(self):
+        return f'Olá{id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Thulio')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    p.name = "Thulio Vinicius"
+    print(p.name)
+    print(p.idade)
