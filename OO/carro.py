154d0e65b1de4d00051322be3bc0997e6a140412
Thulio Vinicius <thuliov@gmail.com>
2020-11-23 21:59:40 -0300

    Inserido class direcao

--- OO/carro.py
+++ OO/carro.py
@@ -1,3 +1,19 @@
+NORTE='Norte'
+SUL='Sul'
+LESTE='Leste'
+OESTE='Oeste'
+
+class direcao:
+    rotacao_direita = {NORTE:LESTE, LESTE:SUL, SUL:OESTE, OESTE:NORTE}
+    rotacao_esquerda = {NORTE:OESTE, OESTE:SUL, SUL:LESTE, LESTE:NORTE}
+
+    def __init__(self):
+        self.valor = NORTE
+    def girar_direita(self):
+        self.valor = self.rotacao_direita[self.valor]
+    def girar_esquecerda(self):
+        self.valor = self.rotacao_esquerda[self.valor]
+
 class motor:
     def __init__(self):
         self.valocidade = 0
